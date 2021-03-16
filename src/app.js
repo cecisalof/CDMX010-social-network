4ce3615d1e84fc983816bc8e6a0a04c7590f8cc4
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-03-16 13:41:06 -0600

    Empty branch

--- src/app.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-// eslint-disable-next-line import/no-cycle
-import { routes, rootDiv, loadFirebase } from './routes.js';
-import * as firebase from './firebase.js';
-
-loadFirebase(firebase);
-const homeView = routes[window.location.pathname];
-homeView(rootDiv, firebase);
--- src/auth/nova.js
+++
@@ -1,9 +0,0 @@
-export const novaApp = (container) => {
-  const html = `
-        <div id="novaAppPage">
-          <img id="inicialLogo" src="resources/novaAppImage.png" alt="Inicial logo" data-action="login">
-        </div>
-        `;
-    // eslint-disable-next-line no-param-reassign
-  container.innerHTML = html;
-};
