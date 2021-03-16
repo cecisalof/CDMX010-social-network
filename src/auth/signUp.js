ee30d3b988c1f9cbdc8bf5e04067a3b61538846a
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-03-16 15:46:20 -0600

    Updating code for Tuesday March, 16th

---
+++ src/auth/signUp.js
@@ -0,0 +1,22 @@
+export const signUp = (container) => {
+  const html = `
+    <div id="signUpDiv">
+        <div>
+        <img id="signUpSnipple" src="resources/garabato.png" alt="logo" class= "link">
+          <h1 id="signUpTitle">Crear una cuenta</h1>
+          <form>
+              <input id="userName" type= "text" placeholder="Tu nombre completo"></input>
+              <input id="userEmail" type= "email" placeholder="Tu correo electrónico"></input>
+              <input id="userPassword" type= "password" placeholder="Tu contraseña"></input>
+              <button class="button" data-action="signUpButton">Crear cuenta</button>
+          </form>
+        </div>
+        
+        <div class="signUpButton">
+        <button data-action="signUpWithGoogle" class="button">Registrarse con Google</button>
+        </div>
+        </div>
+        `;
+    // eslint-disable-next-line no-param-reassign
+  container.innerHTML = html;
+};
---
+++ src/firebase.js
@@ -0,0 +1,48 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// eslint-disable-next-line import/no-cycle
+import { renderPost } from './home.js';
+
+const firebaseConfig = {
+
+  apiKey: 'AIzaSyCE3V_6hn_oiPhJAvfRLJLygBVct9fIZRg',
+  authDomain: 'novaapp-67e15.firebaseapp.com',
+  projectId: 'novaapp-67e15',
+  storageBucket: 'novaapp-67e15.appspot.com',
+  messagingSenderId: '282489634860',
+  appId: '1:282489634860:web:97a4ad5b81716f2b0f5189',
+  measurementId: 'G-N31JQDJTSM',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+// eslint-disable-next-line no-unused-vars
+export const db = firebase.firestore();
+
+// GUARDA INFORMACIÓN DE USUARIIO EN LA BASE DE DATOS.
+export const savePost = (post) => db.collection('newPost')
+  .add({
+    Title: post.title,
+    Subtitle: post.subtitle,
+    Body: post.body,
+    Fecha: Date.now(),
+  });
+
+// TRAE LA DATA DE LA BASE DE DATOS.
+export const getData = () => {
+  const postContainer = document.getElementById('printData');
+  db.collection('newPost').orderBy('Fecha', 'desc')
+    .onSnapshot((querySnapshot) => {
+      postContainer.innerHTML = '';
+      querySnapshot.forEach((doc) => {
+        const dataBase = doc.data();
+        postContainer.innerHTML += renderPost(dataBase);
+        // console.log(dataBase);
+      });
+    });
+};
+
+/*
+export const userAuth = (userEmail, userPassword) => {
+  auth.createUserWithEmailAndPassword(userEmail, userPassword);
+};
+*/
