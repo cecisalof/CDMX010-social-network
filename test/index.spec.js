13b9e06393d051749a1e83782a53dfe14db3bd6c
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-03-24 12:49:51 -0600

    Creating interaction test for delete modal

--- test/index.spec.js
+++ test/index.spec.js
@@ -2,6 +2,7 @@
 
 // import { login } from '../src/lib/login.js';
 import { signIn } from '../src/auth/signIn.js';
+import { deleteConfirmation } from '../src/PostController/deleteConfirmation.js';
 // import { signUp } from '../src/lib/signUp.js';
 // import { eventsController } from '../src/routes.js';
 
@@ -14,4 +15,26 @@ describe('DOM render', () => {
     //signInButton.click();
     expect(container.innerHTML).toMatchSnapshot();
   });
+});
+
+describe('Testing delete confirmation modal template', () => {
+  beforeEach(() => {
+    // const container = document.createElement('div');
+    document.body.innerHTML = '<div id="modalContainer"></div>';
+  });
+  it('should render the modal in the DOM', () => {
+    const modalContainer = document.getElementById('modalContainer');
+    deleteConfirmation(modalContainer);
+    expect(modalContainer.innerHTML).toMatchSnapshot();
+  });
+//   it('should delete de post in firebase', () => {
+//     const confirmButton = document.getElementById('confirm');
+//     let docRef = firestore.doc('newPost/doc');
+//     const deletePost = jest.fn().mockImplementation(() => Promise.resolve(docRef));
+//     confirmButton.click();
+//     docRef.delete().then(() => {
+//       console.log('Document successfully deleted.');
+//       expect(deletePost()).toMatchSnapshot();
+//     });
+//   });
 });
